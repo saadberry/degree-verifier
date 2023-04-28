0dbf1980b6e420af854ace9a6073167c34dec9df
saadberry <saadberry29@gmail.com>
2023-04-28 21:16:08 +0500

    updated config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     localganache: {
-      url: process.env.PROVIDER_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+      url: "HTTP://127.0.0.1:7545",
+      accounts: [`0x${'0a5951b39acaab43fa9f2e5c189fdba6c93a7be9e8b48c5d4e9b7a360c27c1a6'}`]
     }
   }
 };
